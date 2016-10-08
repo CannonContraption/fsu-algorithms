0ea5279ede0098f9b657d706100fb4b1bfe56c89
= <kiernan@kroche.xyz>
2016-10-08 12:13:10 -0400

    fixed implementation

--- string_permutations/inefficient.py
+++ string_permutations/inefficient.py
@@ -4,17 +4,21 @@ import sys
 
 args = sys.argv
 
+isPerm = True
+
 if (len(args) < 3):
 	raise ValueError("Bad arguments.")
 
 if (len(args[1]) != len(args[2])):
 	print(args[1] + " is not a permutation of " + args[2])
-	exit
+	quit()
 
 for char in args[1]:
-	for compchar in args[2]:
-		if (char != compchar):
-			print(args[1] + " is not a permutation of " + args[2])
-			exit
+	if (char not in args[2]):
+		isPerm = False
+		break
 
-print(args[1] + " is a permutation of " + args[2])
+if isPerm:
+	print(args[1] + " is a permutation of " + args[2])
+else:
+	print(args[1] + " is not a permutation of " + args[2])
