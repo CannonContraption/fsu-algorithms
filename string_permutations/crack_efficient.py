75146a11889c78296d58387ed22e83bd922a7daa
Jim Read <jimmydean886@hotmail.com>
2017-03-23 20:15:44 -0400

    Changed to powers of three
    The efficient.py alg works perfectly fine without any holes if you use
    powers of three instead of powers of two. Further, if you check to make
    sure the strings are the same length, it is impossible to do a differing
    string length crack on it. These have been implemented.
    
    Errors the script would output were valueerrors before, which would
    cause a traceback to show too. Now, it prints to stderr and exits with a
    code instead of just using valueErrors.

--- string_permutations/crack_efficient.py
+++ string_permutations/crack_efficient.py
@@ -3,14 +3,14 @@ import sys
 
 for letter in range(ord('a'), ord('z')):
 	for lettertwo in range(ord('A'), ord('Z')):
-		letconv = (letter**2)+(lettertwo**2)
+		letconv = (letter**3)+(lettertwo**3)
 		for i in range(0,int(letconv/2)):
 			conv1 = letconv-i
-			if(math.sqrt(conv1)%2==0):
+			if(conv1**(1./3.)%2==0):
 				conv2 = letconv-(letconv-i)
-				if(math.sqrt(conv2)%2==0):
-					if int(math.sqrt(conv1))<128:
-						if int(math.sqrt(conv2))<128:
+				if(conv2**(1./3.)%2==0):
+					if int(conv1**(1./3.))<128:
+						if int(conv2(1./3.))<128:
 							if(conv1 != letter):
 								if(conv1 != lettertwo):
 									if(conv2 != letter):
