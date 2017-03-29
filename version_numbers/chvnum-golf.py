3932de73721003b040cf2f6f9f08d4cbf06b3f01
Jim Read <jimmydean886@hotmail.com>
2017-03-29 12:33:20 -0400

    Reduced size of golf version

--- version_numbers/chvnum-golf.py
+++ version_numbers/chvnum-golf.py
@@ -11,9 +11,10 @@ if len(sys.argv) == 3:
 		sys.stderr.write("\033[1;31mThese version numbering schemes don't match\033[m\nPlease try a different set, where there is an equal number of sub-versions.\n");
 		exit(1);
 	for i in range(0,len(sys.argv[1].split('.'))):
-		if(int(sys.argv[1].split('.')[i]) < int(sys.argv[2].split('.')[i])): print("Second is newer.");
-		elif (int(sys.argv[1].split('.')[i]) > int(sys.argv[2].split('.')[i])): print("First is newer.");
-		if(int(sys.argv[1].split('.')[i])!=int(sys.argv[2].split('.')[i])): sys.exit(0);
+		a1=int(sys.argv[1].split('.')[i]; a2=int(sys.argv[2].split('.')[i]));
+		if a1 < a2: print("Second is newer.")
+	elif a1 > a2: print("First is newer.")
+		if a1!=a2: sys.exit(0)
 	print("These are the same.")
 else:
 	sys.stderr.write("\033[1;31mInvalid arguments.\033[m\nExiting now.\n")
